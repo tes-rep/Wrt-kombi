9f08c2bab36a5f6201ed0d576cff3819976d8630
Dirgha80 <156698511+Dirgha80@users.noreply.github.com>
2024-12-31 23:42:38 +0700

    Update imagebuilderB.sh

--- imagebuilderB.sh
+++ imagebuilderB.sh
@@ -522,7 +522,7 @@ rebuild_firmware() {
     OPENCLASH+="coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
     MIHOMO+="mihomo luci-app-mihomo"
     PASSWALL+="chinadns-ng resolveip dns2socks dns2tcp ipt2socks microsocks tcping xray-core xray-plugin luci-app-passwall"
-    PACKAGES+=" $OPENCLASH $MIHOMO -$PASSWALL"
+    PACKAGES+=" $OPENCLASH $MIHOMO"
 
   #  PACKAGES+=" $OPENCLASH $MIHOMO"
 
