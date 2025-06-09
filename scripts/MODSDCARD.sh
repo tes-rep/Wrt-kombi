a7bb55bd626ef36bce448fccda980dd7c6fc0085
hj-wrt <mincoek76@gmail.com>
2025-06-09 21:20:37 +0700

    Update MODSDCARD.sh

--- scripts/MODSDCARD.sh
+++ scripts/MODSDCARD.sh
@@ -148,7 +148,7 @@ build_mod_sdcard() {
 
     local kernel
     kernel=$(grep -oP 'k[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)?' <<<"${file_name}")
-    local new_name="${OP_BASE}-${BRANCH}-${suffix}-${kernel}-${TUNNEL}-MODSDCARD-Build-By-Fidz_Xidz-X.img.gz"
+    local new_name="HOUJIE-${suffix}-${kernel}-${TUNNEL}-MODSDCARD-Build-By-Fidz_Xidz-X.img.gz"
 
     mv "${file_name}.gz" "../${new_name}" || {
         error_msg "Failed to rename image file"
