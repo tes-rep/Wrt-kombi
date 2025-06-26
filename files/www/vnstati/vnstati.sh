8db70a0be3a87e060fe49a923dbd4d8a64141a2f
hj-wrt <mincoek76@gmail.com>
2025-06-26 23:24:39 +0700

    Add files via upload

--- files/www/vnstati/vnstati.sh
+++ files/www/vnstati/vnstati.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-vnstati -s -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_s.png
-vnstati -5 -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_5.png
-vnstati -h -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_h.png
-vnstati -d -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_d.png
-vnstati -m -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_m.png
-vnstati -y -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_y.png
-vnstati -t -i br-hotspot -o /www/vnstati/br-hotspot_vnstat_t.png
+vnstati -s -i br-lan -o /www/vnstati/br-lan_vnstat_s.png
+vnstati -5 -i br-lan -o /www/vnstati/br-lan_vnstat_5.png
+vnstati -h -i br-lan -o /www/vnstati/br-lan_vnstat_h.png
+vnstati -d -i br-lan -o /www/vnstati/br-lan_vnstat_d.png
+vnstati -m -i br-lan -o /www/vnstati/br-lan_vnstat_m.png
+vnstati -y -i br-lan -o /www/vnstati/br-lan_vnstat_y.png
+vnstati -t -i br-lan -o /www/vnstati/br-lan_vnstat_t.png
